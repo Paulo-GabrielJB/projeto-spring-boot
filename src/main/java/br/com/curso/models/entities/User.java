1953d5b56bd885d63735128d28e73623a5ac7a58
Paulo Gabriel <pgj1085@gmail.com>
2020-05-08 14:58:53 -0300

    H2 databas, teste profile, JPA

--- src/main/java/br/com/curso/models/entities/User.java
+++ src/main/java/br/com/curso/models/entities/User.java
@@ -4,21 +4,35 @@ package br.com.curso.models.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "TB_USER")
 public class User implements Serializable {
 
     private static final long serialVesionUID = 1L;
 
+    @Id
+    @Column(name = "ID_USER")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Length(min = 5, max = 70)
+    @Column(name = "DS_NAME", nullable = false, length = 70)
     private String name;
+    @Length(min = 10, max = 80)
+    @Column(name = "DS_EMAIL", nullable = false, length = 80)
     private String email;
+    @Length(min = 10, max = 11)
+    @Column(name = "NR_PHONE", nullable = false, length = 11)
     private String phone;
+    @Column(name = "DS_PASSWORD", nullable = false)
     private String password;
 
     @Override
