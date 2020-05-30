5c90c44a10bbdf9fb67d40ed999f0c94d2be862e
Paulo Gabriel <pgj1085@gmail.com>
2020-05-30 20:26:09 -0300

    Entidade Product

--- src/main/java/br/com/curso/config/TestConfig.java
+++ src/main/java/br/com/curso/config/TestConfig.java
@@ -2,10 +2,12 @@ package br.com.curso.config;
 
 import br.com.curso.models.entities.Category;
 import br.com.curso.models.entities.Order;
+import br.com.curso.models.entities.Product;
 import br.com.curso.models.entities.User;
 import br.com.curso.models.entities.enums.OrderStatus;
 import br.com.curso.repositories.CategoryRepository;
 import br.com.curso.repositories.OrderRepository;
+import br.com.curso.repositories.ProductRepository;
 import br.com.curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -38,13 +43,20 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID.getCode(), u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT.getCode(), u1);
 
-        Category c1 = new Category(null, "Eletronics");
-        Category c2 = new Category(null, "Books");
-        Category c3 = new Category(null, "Computers");
+        Category c1 = new Category(null, "Eletronics", null);
+        Category c2 = new Category(null, "Books", null);
+        Category c3 = new Category(null, "Computers", null);
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ispsum dolor sit amet, consectetur.", 90.5, "", null);
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "", null);
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifends maximus tortor, at mollis.", 1250.0, "", null);
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "", null);
+        Product p5 = new Product(null, "Rail for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "", null);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
     }
 }
--- src/main/java/br/com/curso/models/entities/Product.java
+++ src/main/java/br/com/curso/models/entities/Product.java
@@ -14,26 +14,33 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "TB_CATEGORY")
-public class Category implements Serializable {
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable {
 
     private static final long serialVesionUID = 1L;
 
     @Id
-    @Column(name = "ID_CATEGORY")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PRODUCT")
     private Long id;
-    @Column(name = "DS_CATEGORY")
+    @Column(name = "NM_PRODUCT")
     private String name;
+    @Column(name = "DS_PRODUCT")
+    private String description;
+    @Column(name = "VL_PRODUCT")
+    private Double price;
+    @Column(name = "IMG_PRODUCT")
+    private String imgUrl;
+
     @Transient
-    private Set<Product> products = new HashSet<>();
+    private Set<Category> categories = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id);
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
     }
 
     @Override
